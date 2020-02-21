441830d8f9ded0665b4bc3f9286d15e05054f9b9
arcanlte <pinangayjoel@gmail.com>
2020-02-21 09:03:22 -0500

    Component aboutMe.js initiated

---
+++ src/skills.js/aboutMe.js
@@ -0,0 +1,15 @@
+import react from "react";
+
+function AboutMe() {
+  return (
+    <div className="aboutMe">
+      <h2>About Me</h2>
+      <p>
+        I am a hard-working web developer who seeks for a full-time position
+        where I can apply my knowledge and skills for professional growth. With
+        my passion to learn new skills together with my drive towards personal
+        growth I believe that I am a right fit to your growth driven company.
+      </p>
+    </div>
+  );
+}
